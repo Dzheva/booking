4991d7235c95d91682d5275a04eb4f68055f423b
Bohdan <13829816-brightly-shining@users.noreply.gitlab.com>
2023-11-05 00:23:31 +0100

    ⚗️ Implement resource handler

---
+++ src/main/java/utilities/ResourceHandler.java
@@ -0,0 +1,31 @@
+package utilities;
+
+import java.io.*;
+import java.nio.file.Path;
+
+@SuppressWarnings("CallToPrintStackTrace")
+public final class ResourceHandler {
+    public static void saveData(String filename, Object object) {
+        try (FileOutputStream fileStream = new FileOutputStream(getResourcePath(filename));
+             ObjectOutputStream objectStream = new ObjectOutputStream(fileStream)) {
+            objectStream.writeObject(object);
+        } catch (IOException exception) {
+            exception.printStackTrace();
+        }
+    }
+
+    public static Object loadData(String filename) {
+        try (FileInputStream fileStream = new FileInputStream(getResourcePath(filename));
+             ObjectInputStream objectStream = new ObjectInputStream(fileStream)) {
+            return objectStream.readObject();
+        } catch (IOException | ClassNotFoundException exception) {
+            exception.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String getResourcePath(String fileName) {
+        return Path.of("src", "main", "resources", fileName)
+                .toAbsolutePath().toString();
+    }
+}
