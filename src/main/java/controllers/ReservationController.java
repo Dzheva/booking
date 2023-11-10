27d12c115775c029f94d77de94d116e0390551e9
Ihor Dzheva <idzheva@gmail.com>
2023-11-10 15:41:40 +0200

    updated Reservation Controller

--- src/main/java/controllers/ReservationController.java
+++ src/main/java/controllers/ReservationController.java
@@ -1,5 +1,6 @@
 package controllers;
 
+import models.Flight;
 import models.Reservation;
 import models.User;
 import services.FlightService;
@@ -18,18 +19,38 @@ public class ReservationController {
     }
 
     public void addReservation(Reservation reservation){
-            flightService.decreaseSeats(reservation.getFlightId(),reservation.getPassengers().size());
-            reservationService.addReservation(reservation);
+            List<Flight> flightList = flightService.getAllFlights();
+
+            if(flightList.contains(getFlightByReservation(reservation))){
+                flightService.decreaseSeats(getIdFlightByReservation(reservation), reservation.getPassengers().size());
+                reservationService.addReservation(reservation);
+                System.out.println("The reservation is completed.");
+            } else {
+                System.out.println("This flight doesn't exist.");
+            }
     }
 
     public void cancelReservation(Reservation reservation){
-        flightService.increaseSeats(reservation.getFlightId(), reservation.getPassengers().size());
-        deleteReservationById(reservation.getId());
+            List<Reservation> reservationList = reservationService.getAllReservation();
+
+            if(reservationList.contains(reservation)){
+                flightService.increaseSeats(reservation.getFlightId(), reservation.getPassengers().size());
+                deleteReservationById(reservation.getId());
+                System.out.println("The reservation is canceled.");
+            } else {
+                System.out.println("This reservation doesn't exist.");
+            }
     }
 
     public void deleteReservationById(int id){
         reservationService.deleteReservationById(id);
     }
 
+    public Flight getFlightByReservation(Reservation reservation){
+        return flightService.getFlightById(getIdFlightByReservation(reservation));
+    }
 
+    public int getIdFlightByReservation(Reservation reservation){
+        return reservation.getFlightId();
+    }
 }
