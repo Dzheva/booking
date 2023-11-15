79d940d2159d3d002e676fbb01e00efeca1336a5
Ihor Dzheva <idzheva@gmail.com>
2023-11-15 16:47:01 +0200

    added tests for FlightDao and FlightService

--- src/test/java/daos/FlightDAOTest.java
+++ src/test/java/daos/FlightDAOTest.java
@@ -1,18 +1,74 @@
 package daos;
 
+import constants.ResourceName;
+import models.Flight;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utilities.ResourceHandler;
+
+import java.io.File;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class FlightDAOTest {
+    private FlightDAO flightDAO;
+    private Flight flight;
+
+    @BeforeEach
+    void setUp() {
+        flightDAO = new FlightDAO();
+        LocalDateTime departureTime = LocalDateTime.of(2023, 10, 15, 14, 30);
+        LocalDateTime arriveTime = LocalDateTime.of(2023, 10, 15, 18, 15);
+        flight = new Flight(flightDAO.getNextId(), "New York", "Milan", departureTime, arriveTime, 50);
+    }
 
     @Test
     void create() {
+        flightDAO.create(flight);
+        assertAll(
+                () -> assertTrue(flightDAO.getAll().contains(flight)),
+                () -> assertFalse(flightDAO.getAll().contains(null))
+        );
     }
 
     @Test
     void get() {
+            flightDAO.create(flight);
+            assertEquals(flightDAO.get(flight.getId()), flight);
     }
 
     @Test
     void getAll() {
+        //We generated 50 flights previously.
+        flightDAO.create(flight);
+
+        assertAll(
+                () -> assertTrue(flightDAO.getAll().contains(flight)),
+                () -> assertEquals(flightDAO.getNextId()-1, flightDAO.getAll().size())
+        );
+    }
+
+    @Test
+    void getNextId(){
+        int actual = flightDAO.getNextId();
+        int expected = flightDAO.getAll().size()+2;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void saveAll(){
+        flightDAO.create(flight);
+        flightDAO.saveAll();
+        int size1 = flightDAO.getAll().size();
+
+        FlightDAO flightDAO2 = new FlightDAO();
+        int size2 = flightDAO2.getAll().size();
+
+        assertEquals(size1, size2);
     }
 }
--- src/test/java/services/FlightServiceTest.java
+++ src/test/java/services/FlightServiceTest.java
@@ -1,26 +1,90 @@
 package services;
 
+import models.Flight;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class FlightServiceTest {
+    private FlightService flightService;
+    @BeforeEach
+    void setUp(){
+        flightService = new FlightService();
+    }
 
     @Test
     void getAllFlights() {
+        List<Flight> flights = flightService.getAllFlights();
+        assertNotNull(flights);
+        assertFalse(flights.isEmpty());
+        assertTrue(flights.size() > 1);
     }
 
     @Test
     void getFlight() {
+        //Flights ID starts from 1
+        assertAll(
+                () -> assertNull(flightService.getFlight(0)),
+                () -> assertNotNull(flightService.getFlight(1)),
+                () -> assertNotNull(flightService.getFlight(2))
+        );
     }
 
     @Test
     void findFlights() {
+        String destination = "OneWayTicket";
+        int numberOfPassengers = 2;
+
+        flightService.createFlight("Origin1", destination, LocalDateTime.now(), LocalDateTime.now().plusHours(2), 12);
+        flightService.createFlight("Origin2", "AnotherDestination", LocalDateTime.now(), LocalDateTime.now().plusHours(2), 8);
+        flightService.createFlight("Origin3", destination, LocalDateTime.now(), LocalDateTime.now().plusHours(2), 15);
+
+        List<Flight> foundFlights = flightService.findFlights(destination, numberOfPassengers);
+        assertNotNull(foundFlights);
+
+        assertTrue(foundFlights.stream().allMatch(flight ->
+                flight.getDestination().equalsIgnoreCase(destination) &&
+                        flight.getSeatsAvailable() >= numberOfPassengers + 1));
+
+        assertEquals(foundFlights.size(), 2);
     }
 
     @Test
     void createFlight() {
+        String origin = "Origin";
+        String destination = "Destination";
+        LocalDateTime departureTime = LocalDateTime.now();
+        LocalDateTime arrivalTime = departureTime.plusHours(2);
+        int seatsAvailable = 100;
+
+        flightService.createFlight(origin, destination, departureTime, arrivalTime, seatsAvailable);
+
+        Flight createdFlight = flightService.getFlight(flightService.getAllFlights().size());
+
+        assertAll(
+                () -> assertEquals(origin, createdFlight.getOrigin()),
+                () -> assertEquals(destination, createdFlight.getDestination()),
+                () -> assertEquals(departureTime, createdFlight.getDepartureTime()),
+                () -> assertEquals(arrivalTime, createdFlight.getArrivalTime()),
+                () -> assertEquals(seatsAvailable, createdFlight.getSeatsAvailable())
+        );
     }
 
     @Test
     void generateAndAddFlights() {
+        int count = 5;
+        int initialSize = flightService.getAllFlights().size();
+
+        flightService.generateAndAddFlights(count);
+
+        List<Flight> flights = flightService.getAllFlights();
+        int finalSize = flights.size();
+
+        assertEquals(finalSize, initialSize + count);
     }
 }
