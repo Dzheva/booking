480ccdda5a335700020d62e22ccb3b609cdb0216
Ihor Dzheva <idzheva@gmail.com>
2023-11-16 15:56:36 +0200

    added tests for UserDAO&Service

--- src/test/java/daos/UserDAOTest.java
+++ src/test/java/daos/UserDAOTest.java
@@ -1,18 +1,66 @@
 package daos;
 
+import models.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class UserDAOTest {
 
+    private UserDAO userDAO;
+
+    private User testUser;
+
+    @BeforeEach
+    void setUp(){
+        userDAO = new UserDAO();
+        testUser = new User(userDAO.getNextId(), "abc", "123", "TestName", "TestLastName");
+        userDAO.create(testUser);
+    }
+
     @Test
     void create() {
+        assertAll(
+                () -> assertTrue(userDAO.getAll().contains(testUser)),
+                () -> assertFalse(userDAO.getAll().contains(null))
+        );
     }
 
     @Test
     void get() {
+        assertAll(
+                () -> assertEquals(userDAO.get(testUser.id()),testUser),
+                () -> assertEquals(userDAO.get(testUser.id()).firstName(), testUser.firstName()),
+                () -> assertEquals(userDAO.get(testUser.id()).lastName(), testUser.lastName())
+        );
     }
 
     @Test
     void getAll() {
+        assertAll(
+                () -> assertTrue(userDAO.getAll().contains(testUser)),
+                () -> assertEquals(userDAO.getNextId()-1, userDAO.getAll().size())
+        );
     }
+
+    @Test
+    void getNextId(){
+        int actual = userDAO.getNextId();
+        int expected = userDAO.getAll().size() + 1;
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void saveAll(){
+        userDAO.saveAll();
+        int size1 = userDAO.getAll().size();
+
+        UserDAO userDAO2 = new UserDAO();
+        int size2 = userDAO2.getAll().size();
+
+        assertEquals(size1, size2);
+    }
+
 }
--- src/test/java/services/UserServiceTest.java
+++ src/test/java/services/UserServiceTest.java
@@ -1,14 +1,44 @@
 package services;
 
+import models.User;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 class UserServiceTest {
+    UserService userService;
+    String login = "login1";
+    String password = "pass2";
+    String firstName = "Bob";
+    String lastName = "Franklin";
+
+    Optional<User> createdUser;
+
+    @BeforeEach
+    void setUp(){
+        userService = new UserService();
+        userService.createUser(login, password, firstName, lastName);
+        createdUser = userService.findUser(login, password);
+    }
 
     @Test
     void findUser() {
+        assertAll(
+                () -> assertFalse(createdUser.isEmpty()),
+                () -> assertTrue(createdUser.get().firstName().equals(firstName)),
+                () -> assertTrue(createdUser.get().lastName().equals(lastName))
+        );
     }
 
     @Test
     void createUser() {
+        assertAll(
+                () -> assertTrue(createdUser.get().login().equals(login)),
+                () -> assertTrue(createdUser.get().password().equals(password)),
+                () -> assertTrue(createdUser.get().firstName().equalsIgnoreCase("BOB"))
+        );
     }
 }
